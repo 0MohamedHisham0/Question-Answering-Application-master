7b883840eb5dee89bbda9fcb096845f04a747ead
0MohamedHisham0 <MohamedDal4300@gmail.com>
2022-01-04 02:36:59 +0200

    ../models

--- search/api.py
+++ search/api.py
@@ -72,7 +72,33 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
             # change path to pdfs folder
             df = pdf_converter(directory_path='./media')
             # print("after data frame", df)
-            cdqa_pipeline = QAPipeline(reader='../models/distilbert_qa.joblib', max_df=1.0)
+
+            try:
+                cdqa_pipeline = QAPipeline(reader='models/distilbert_qa.joblib', max_df=1.0)
+            except:
+                print("Error Models")
+
+            try:
+                cdqa_pipeline = QAPipeline(reader='/models/distilbert_qa.joblib', max_df=1.0)
+            except:
+                print("Error Models")
+
+            try:
+                cdqa_pipeline = QAPipeline(reader='./models/distilbert_qa.joblib', max_df=1.0)
+            except:
+                print("Error Models")
+
+            try:
+                cdqa_pipeline = QAPipeline(reader='../models/distilbert_qa.joblib', max_df=1.0)
+            except:
+                print("Error Models")
+
+            try:
+                cdqa_pipeline = QAPipeline(reader='\\models/distilbert_qa.joblib', max_df=1.0)
+            except:
+                print("Error Models")
+
+
             print("first pipeline")
             cdqa_pipeline.fit_retriever(df=df)
             print("second pipeline")
